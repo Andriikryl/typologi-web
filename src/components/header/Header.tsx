ad1ce6fa68ec021abf6aa8a2e6ca56e52cf2cd26
Andriikryl <andreyk557@gmail.com>
2024-01-02 13:56:24 +0200

    add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import styles from "./style.module.css";
+import { Container } from "../container/Container";
+import Link from "next/link";
+import Image from "next/image";
+
+const dataNavLinks = [
+  {
+    id: 1,
+    title: "Products",
+    href: "/products",
+  },
+  {
+    id: 2,
+    title: "Diagnostic",
+    href: "/diagnostic",
+  },
+  {
+    id: 3,
+    title: "ABOUT",
+    href: "/about",
+  },
+];
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Container>
+        <div className={styles.header__box}>
+          <nav>
+            <ul>
+              {dataNavLinks.map(({ id, title, href }) => {
+                return (
+                  <li key={id}>
+                    <Link href={href}>{title}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+          <Link href={"/"}>
+            <Image
+              src="/logo/logo.svg"
+              width={140}
+              height={51}
+              alt="Typology paris"
+            />
+          </Link>
+          <div>
+            <ul>
+              <li>
+                <Link href={"/"}>
+                  <Image
+                    src="/navigateIcons/search.svg"
+                    width={16}
+                    height={16}
+                    alt="Typology paris"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"}>Global</Link>
+              </li>
+              <li>
+                <Link href={"/"}>Login</Link>
+              </li>
+              <li>
+                <Link href={"/"}>
+                  <Image
+                    src="/navigateIcons/heart.svg"
+                    width={16}
+                    height={16}
+                    alt="Typology paris"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"}>cart</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+}
