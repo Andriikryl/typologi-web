2fa39d6ec5e1ef1e742858a8ab98ec7e0497d3b0
Andriikryl <andreyk557@gmail.com>
2024-01-02 15:26:46 +0200

    add prewe component

---
+++ src/components/prewe/Prewe.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./style.module.css";
+
+export default function Prewe() {
+  return (
+    <div className={styles.prew}>
+      <p className={styles.prew__title}>
+        Discover our newly reformulated SPF 50 sunscreens for face and body
+      </p>
+    </div>
+  );
+}
